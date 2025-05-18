6dcf83db5e67dd82f511ba3ccf428b67faf17be6
MacCoy Smith <d.maccoy.smith@gmail.com>
2025-05-17 22:01:27 -0600

    add support for external programs

--- src/main/java/BuiltIn.java
+++ src/main/java/BuiltIn.java
@@ -11,4 +11,36 @@ public enum BuiltIn {
             default -> null;
         };
     }
+
+    public static void execute(BuiltIn command, String args) {
+        switch (command) {
+            case EXIT:
+                System.exit(0);
+            case ECHO:
+                System.out.println(args != null ? args : "");
+                break;
+            case TYPE:
+                if (args == null || args.isBlank()) break;
+                
+                String cmd = args.split(" ")[0];
+
+                // check if builtin
+                if (parse(cmd) != null) {
+                    System.out.println(cmd + " is a shell builtin");
+                    break;
+                }
+
+                // check if executable found in PATH
+                String executablePath = PathScanner.findExecutablePath(cmd);
+                if (executablePath != null) {
+                    System.out.println(cmd + " is " + executablePath);
+                    break;
+                }
+
+                System.out.println(cmd + ": not found");
+                break;
+            default:
+                System.out.println(command + ": command not found");
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,41 +10,82 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-
             if (input.isBlank()) continue;
-
             String[] tokens = input.split(" ", 2);
-            BuiltIn command = BuiltIn.parse(tokens[0]);
 
-            switch (command) {
-                case EXIT:
-                    System.exit(0);
-                case ECHO:
-                    System.out.println(tokens[1]);
-                    continue;
-                case TYPE:
-                    String cmd = tokens[1].split(" ")[0];
-
-                    // check if builtin
-                    BuiltIn builtIn = BuiltIn.parse(cmd);
-                    if (builtIn != null) {
-                        System.out.println(cmd + " is a shell builtin");
-                        continue;
-                    }
-
-                    // check if executable found in PATH
-                    String executablePath = PathScanner.findExecutablePath(cmd);
-                    if (executablePath != null) {
-                        System.out.println(cmd + " is " + executablePath);
-                        continue;
-                    }
-
-                    System.out.println(cmd + ": not found");
-                    continue;
-                case null:
-                default:
-                    System.out.println(tokens[0] + ": command not found");
+            // check if command is built-in
+            BuiltIn builtIn = BuiltIn.parse(tokens[0]);
+            if (builtIn != null) {
+                String arguments = tokens.length > 1 ? tokens[1] : null;
+                BuiltIn.execute(builtIn, arguments);
+                continue;
             }
+
+            // check if command is external
+            String executablePath = PathScanner.findExecutablePath(tokens[0]);
+            if (executablePath != null) {
+                List<String> command = new ArrayList<>();
+                command.add(executablePath);
+                if (tokens.length > 1) {
+                    final List<String> arguments = parseArgs(tokens[1]);
+                    command.addAll(arguments);
+                }
+                try {
+                    Process process = new ProcessBuilder(command).start();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    int exitCode = process.waitFor();
+                    if (exitCode != 0) {
+                        BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                        String errorLine;
+                        while ((errorLine = error.readLine()) != null) {
+                            System.err.println(errorLine);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error executing " + tokens[0] + ": " + e.getMessage());
+                }
+                continue;
+            }
+
+            System.out.println("Unknown command: " + tokens[0]);
         }
+    }
+
+    private static List<String> parseArgs(String argsString) {
+        argsString = argsString.trim().replace('\'', '\"');
+        List<String> arguments = new ArrayList<>();
+        int index = 0;
+        StringBuilder builder = new StringBuilder();
+        boolean insideString = false;
+        while (index < argsString.length()) {
+            char charAtIndex = argsString.charAt(index);
+            switch (charAtIndex) {
+                case '\"':
+                    if (insideString) {
+                        arguments.add(builder.toString());
+                        builder = new StringBuilder();
+                    } else {
+                        insideString = true;
+                    }
+                    break;
+                case ' ':
+                    if (insideString) {
+                        builder.append(charAtIndex);
+                    } else {
+                        arguments.add(builder.toString());
+                        builder = new StringBuilder();
+                    }
+                    break;
+                default:
+                    builder.append(charAtIndex);
+            }
+            index++;
+        }
+        if (!builder.isEmpty()) arguments.add(builder.toString());
+        return arguments;
     }
 }
