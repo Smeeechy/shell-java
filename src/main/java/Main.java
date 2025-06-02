3f1d33ca7a26b1c312793addb6ce28fb245d8c9f
MacCoy Smith <d.maccoy.smith@gmail.com>
2025-06-01 20:28:57 -0600

    prevent jline parser from modifying input text

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.DefaultParser;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
     private static final Shell SHELL = new Shell();
+    private static final DefaultParser PARSER = new DefaultParser();
 
     public static void main(String[] args) throws Exception {
-        try (Terminal terminal = TerminalBuilder.terminal()) {
-            LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).build();
+        Logger.getLogger("org.jline").setLevel(Level.SEVERE);
+        try (Terminal terminal = TerminalBuilder.builder().system(true).build()) {
+            PARSER.setEscapeChars(new char[0]);
+            LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).parser(PARSER).build();
             while (true) {
                 String input = lineReader.readLine("$ ");
                 if (input.isBlank()) continue;
